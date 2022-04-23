a2686c80d843897edcac3fc8f63f5a5e7383620b
KRuslanM <kasimoffrus@yandex.ru>
2022-04-23 21:13:14 +0300

    create project file

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+    return(
+        <header className='header'>
+            <img
+                src='https://image.similarpng.com/very-thumbnail/2020/11/SM-Company-name-lettering-logo-design-on-transparent-background-PNG.png'/>
+        </header>
+    )
+}
+
+export default Header;
