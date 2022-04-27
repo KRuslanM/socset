9fbf622f47dab66a7b57c177a5aa6233956738cc
KRuslanM <kasimoffrus@yandex.ru>
2022-04-27 20:41:26 +0300

    page Dialogs, props for dialogs

--- src/App.js
+++ src/App.js
@@ -11,28 +11,21 @@ import Settings from "./components/Settings/Settings";
 
 const App = (props) => {
     return (
-<BrowserRouter>
-
-
-        <div className='app-wrapper'>
-            <Header/>
-            <Navbar/>
-            <div className='app-wrapper-content'>
-
-<Routes>
-
-                <Route path='/dialogs' element={<Dialogs />}/>
-                <Route path='/profile' element={<Profile />}/>
-    <Route path='/news' element={<News />}/>
-    <Route path='/music' element={<Music />}/>
-    <Route path='/settings' element={<Settings />}/>
-
-</Routes>
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <Header/>
+                <Navbar/>
+                <div className='app-wrapper-content'>
+                    <Routes>
+                        <Route path='/dialogs/*' element={<Dialogs/>}/>
+                        <Route path='/profile' element={<Profile/>}/>
+                        <Route path='/news' element={<News/>}/>
+                        <Route path='/music' element={<Music/>}/>
+                        <Route path='/settings' element={<Settings/>}/>
+                    </Routes>
+                </div>
             </div>
-
-        </div>
-
-</BrowserRouter>
+        </BrowserRouter>
 
 
     )
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,9 +1,35 @@
 import s from './Dialogs.module.css';
+import {NavLink} from "react-router-dom";
+
+const DialogItem = (props) => {
+    let path ="/dialogs/" + props.id;
+    return(
+        <div className={s.dialog + ' ' + s.active}>
+            <NavLink to={path}>{props.name}</NavLink>
+        </div>
+    )
+}
+const Message =(props) => {
+    return(
+        <div className={s.message}>{props.message}</div>
+    )
+}
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <DialogItem name="Dima" id="1" />
+                <DialogItem name="Sasha" id="2" />
+                <DialogItem name="Victor" id="2" />
+                <DialogItem name="Valera" id="4" />
+            </div>
+            <div className={s.messages}>
+                <Message message = "Hi"/>
+                <Message message = "How are you IT"/>
+                <Message message = "Yo"/>
+                <Message message = "You"/>
+            </div>
         </div>
     )
 }
