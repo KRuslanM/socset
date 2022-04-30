c5417ebf329e34366962c37853f00f4d41851ffd
KRuslanM <kasimoffrus@yandex.ru>
2022-04-30 15:31:57 +0300

    Lesson 29 - Data set from index.js to new file redux.js

--- src/index.js
+++ src/index.js
@@ -3,33 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import state from "./redux/State";
 
-let posts = [
-    {id: '1', message: 'Hey, how are you?', likesCount: 12},
-    {id: '2', message: 'Hello, i am fine', likesCount: 11},
-    {id: '3', message: 'Hello, Dude', likesCount: 16}
-]
-let dialogs = [
-    {id: '1', name: 'Dima'},
-    {id: '2', name: 'Sasha'},
-    {id: '3', name: 'Victor'},
-    {id: '4', name: 'Valera'}
-]
-
-let messages = [
-    {id: '1', message: 'Hi'},
-    {id: '2', message: 'How are you IT'},
-    {id: '3', message: 'Yo'},
-    {id: '4', message: 'You'}
-]
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
   <React.StrictMode>
-    <App posts={posts} dialogs={dialogs} messages={messages}/>
+
+    <App state={state} />
 
   </React.StrictMode>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/redux/State.js
@@ -0,0 +1,25 @@
+let state = {
+    profilesPage: {
+        posts: [
+            {id: '1', message: 'Hey, how are you?', likesCount: 12},
+            {id: '2', message: 'Hello, i am fine', likesCount: 11},
+            {id: '3', message: 'Hello, Dude', likesCount: 16}
+        ]
+    },
+   dialogsPage: {
+        dialogs: [
+            {id: '1', name: 'Dima'},
+            {id: '2', name: 'Sasha'},
+            {id: '3', name: 'Victor'},
+            {id: '4', name: 'Valera'}
+        ],
+        messages: [
+            {id: '1', message: 'Hi'},
+            {id: '2', message: 'How are you IT'},
+            {id: '3', message: 'Yo'},
+            {id: '4', message: 'You'}
+        ]
+    }
+}
+
+export default state;
